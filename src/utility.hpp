65c9c5ca4478d508a3abd0822787eaea4e923585
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2019-11-07 11:45:56 +0200

    Made the library header-only

--- src/utility.hpp
+++ src/utility.hpp
@@ -35,7 +35,20 @@ namespace non_std {
 	// auto foo = string_split("Number #{}: ", "{}");
 	//
 	// returns: std::vector<std::string> {"Number #", ": "}
-	std::vector<std::string> string_split(std::string s, std::string delimiter);
+	std::vector<std::string> string_split(std::string s, std::string delimiter) {
+		size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+		std::string token;
+		std::vector<std::string> res;
+
+		while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+			token = s.substr(pos_start, pos_end - pos_start);
+			pos_start = pos_end + delim_len;
+			res.push_back(token);
+		}
+
+		res.push_back(s.substr(pos_start));
+		return res;
+	}
 
 	// Simple functions that take individual parameters
 	// and return a result with them; eg. reusable lambdas
